c5cc98c15a6236f569dc17ade51ec9130a7bae1c
beniezsche <bikramankarmakar10@gmail.com>
2023-11-03 18:56:30 +0530

    can extract text from Hindustan Times

--- content.js
+++ content.js
@@ -3,15 +3,68 @@
 const textToReplace = "The Times Of India";
 const replacementText = "Replaced Text";
 
+const OPEN_AI_API_KEY = "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";
+
+const endpoint = 'https://api.openai.com/v1/chat/completions';
+
 // Function to replace text
 function replaceText() {
-  const elements = document.body.getElementsByTagName("*");
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    if (element.nodeType === Node.TEXT_NODE) {
-      element.textContent = element.textContent.replace(textToReplace, replacementText);
-    }
-  }
+
+    //   // Example: Wait for DOMContentLoaded
+    // document.addEventListener("DOMContentLoaded", function () {
+    //   // Your code here
+
+    // });
+
+    let elements = document.body.querySelectorAll('#topnews h3.hdg3'); //h3.hdg3');
+
+    elements.forEach((elem) => {
+      // console.log(elem);
+
+      if(elem.nodeType === Node.TEXT_NODE) {
+        console.log("text");
+      }
+
+      let words = elem.innerText.split(' ');
+      let wordSize = words.length;
+
+      console.log(words[wordSize - 1]);
+
+      // elem.innerText = elem.innerText + " and your girlfriend is a whore."
+    });
+
+    const requestData = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.'
+        },
+        {
+          role: 'user',
+          content: 'Compose a poem that explains the concept of recursion in programming.'
+        }
+      ]
+    };
+    
+    // fetch(endpoint, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': `Bearer ${OPEN_AI_API_KEY}`
+    //   },
+    //   body: JSON.stringify(requestData)
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   // Handle the API response data here
+    //   console.log(data);
+    // })
+    // .catch(error => {
+    //   // Handle errors
+    //   console.error(error);
+    // });
+
 }
 
 // Call the replaceText function
